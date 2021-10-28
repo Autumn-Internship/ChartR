02e2b2f453c409b54fdc948b3fdd69a6ae681e34
Grigor Andrei <andrei.grigor@endava.com>
2021-10-28 12:03:38 +0300

    Change from compare all to compare two

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -1,22 +1,59 @@
 // set API key for the stocks API.
 const API_KEY='XaDgjz85YrsHihbcukvR';
+
+const cmp1 = document.getElementById(`cmp1`);
+const cmp2 = document.getElementById(`cmp2`);
+
 let country;
-let euro_dnk;
-let euro_dnk_high=[];
+let compared1;
+let compared2;
+let data1;
+let high1=[];
+let data2;
+let high2=[];
+const idDNK=9;
+const idBGL=629;
+const idCRK=659;
+const idSEK=68;
+const idCZK=645;
+const idHUF=661;
+const idPLZ=704;
+const idRON=707;
 // get the name of the stock you want to see details for
+
+function getCMP(){
+    compared1 = cmp1.options[select.selectedIndex].value;
+    compared2 = cmp2.options[select.selectedIndex].value;
+}
 
 
 async function getData(){
+    //set1
     await fetch(`https://fcsapi.com/api-v3/forex/history?id=9&period=month&access_key=${API_KEY}`)
     .then(res => res.json())
     .then(data => {
-        euro_dnk= data['response'];
+        data1= data['response'];
     })
     .catch(err => console.log(err));
 
-    for(const [key, value] of Object.entries(euro_dnk)){
-        euro_dnk_high= [...euro_dnk_high, value.h];
+
+    // parse it in a format easy to use in chartJS
+    // took the data from each candle and saved the high value in conversion_high
+    for(const [key, value] of Object.entries(data1)){
+        high1= [...high1, value.h];
     }
-    console.log(euro_dnk_high);
+
+    //set2
+    await fetch(`https://fcsapi.com/api-v3/forex/history?id=629&period=month&access_key=${API_KEY}`)
+    .then(res => res.json())
+    .then(data => {
+        data2= data['response'];
+    })
+    .catch(err => console.log(err));
+
+
+    for(const [key, value] of Object.entries(data2)){
+        high2= [...high2, value.h];
+    }
+
 }
-// parse it in a format easy to use in chartJS
