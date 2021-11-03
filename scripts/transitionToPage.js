c3483d2b79a9c2f815ba60155ff9078ea5dda05c
SaleRazvan <salerazvan@yahoo.com>
2021-11-03 20:48:14 +0200

    Added metro page, tried to add animation for current Tab

--- scripts/transitionToPage.js
+++ scripts/transitionToPage.js
@@ -1,12 +1,27 @@
-function transitionToPage(href) {
-    document.querySelector('body').style.opacity = 0;
-    document.querySelector('body').style.transition = '.5s';
-    setTimeout(function() { 
-        window.location.href = href
-    }, 500)
+function memorize(current, next) {
+  localStorage.setItem("current", current);
+  localStorage.setItem("next", next);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('body').style.opacity = 1
-    document.querySelector('body').style.transition = '.5s'
-})
+function transitionToPage(href) {
+  document.querySelector("body").style.opacity = 0;
+  document.querySelector("body").style.transition = ".5s";
+  setTimeout(function () {
+    window.location.href = href;
+  }, 500);
+}
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   document.querySelector("body").style.opacity = 1;
+//   document.querySelector("body").style.transition = ".5s";
+//   if (document.URL != localStorage.getItem("current")) {
+//     document
+//       .querySelector(localStorage.getItem("next"))
+//       .classList.add("active");
+//     document
+//       .querySelector(
+//         `.${localStorage.getItem("current").slice(57).replace(".html", "")}`
+//       )
+//       .classList.remove("active");
+//   }
+// });
